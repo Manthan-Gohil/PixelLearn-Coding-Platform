eb45643f4857c2c30d54d1f7e5e615b5594b05ff
Manthan-Gohil <manthangohil22577@gmail.com>
2026-04-02 20:42:08 +0530

    deleting unnecessary files

--- parse_wandbox.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('wandbox_utf8.json', 'utf8'));
-const pyCompilers = data.filter(c => c.language === 'Python');
-console.log(pyCompilers.map(c => c.name));
